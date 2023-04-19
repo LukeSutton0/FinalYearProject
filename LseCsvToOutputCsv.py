53bd073a9858d02a7d6ccc453ede9ef2fd4e4865
Luke <80263461+LukeSutton0@users.noreply.github.com>
2023-04-19 18:57:56 +0100

    Data preparation started, looking at main feature for each column

--- LseCsvToOutputCsv.py
+++ LseCsvToOutputCsv.py
@@ -6,21 +6,20 @@ import pandas as pd
 import yfinance
 # Read the CSV file into a list of dictionaries
 
-
-
-def openFileToRead():
-    tickerList = []
-    with open(os.getcwd()+'\\MainCsvs\\New issues and IPOs_37.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row[5] == 'TIDM':
-                pass
-            else:
-                tickerList.append(row[5] + '.L')
-    return tickerList
-
-
-
+# def openFileToRead():
+#     tickerList = []
+#     directory = os.getcwd() + '\\MainData'
+#     for filename in os.listdir(directory):
+#         # Check if the file name starts with "New issues"
+#         if filename.startswith("New issues"):
+#             with open(filename, 'r') as file:
+#                 reader = csv.reader(file)
+#                 for row in reader:
+#                     if row[5] == 'TIDM':
+#                         pass
+#                     else:
+#                         tickerList.append(row[5] + '.L')
+#     return tickerList
 
 # def adjustDayList(dayList):
 #     dayListAlt = []
@@ -46,7 +45,6 @@ def endDateCalculate(datetimeObj,dayList):
 def adjCloseCalc(mainDf,index,dataFromYahoo,daysToAdd):
     date = dataFromYahoo.index.min()
     try:
-        #print(date)
         date = date + pd.Timedelta(days=daysToAdd-1)
         date_str = date.strftime("%Y-%m-%d")
         currentDate = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -63,9 +61,7 @@ def adjCloseCalc(mainDf,index,dataFromYahoo,daysToAdd):
                         count2 = 0
                         for row in dataFromYahoo.index:
                             if count-1 == count2:
-                                #print(row)
                                 mainDf.at[index.Index, column] = dataFromYahoo.loc[row]['Adj Close']
-                                #print current row
                                 break
                             count2+=1
                         mainDf.at[index.Index, column] = dataFromYahoo.loc[aDate]['Adj Close']
@@ -79,31 +75,41 @@ def adjCloseCalc(mainDf,index,dataFromYahoo,daysToAdd):
 
 
 def main():
-        pandas.set_option('display.max_columns', None)
-        df = pd.read_excel(os.getcwd() + '\\MainCsvs\\New issues and IPOs_37.xlsx', sheet_name='New Issues and IPOs',header=7)
-        rows_to_drop = df[df['LSE IPO'] == "Not IPO"].index
-        df.drop(rows_to_drop, inplace=True)
-        df.to_csv(os.getcwd() + '\\MainCsvs\\New issues and IPOs_37.csv', index=False)
-        mainDf = pandas.read_csv(os.getcwd() + '\\MainCsvs\\New issues and IPOs_37.csv',encoding='latin1',header=0)
-        for index in mainDf.itertuples():
-            try:
-               if index.TIDM != "": #if ticker exists
-                    dataFromYahoo = yfinance.download(index.TIDM+".L", period="max", repair=True, interval="1d",progress=False)
-                    if dataFromYahoo.empty:
-                        pass
-                    else:
-                        mainDf.at[index.Index, 'Initial Trading Open'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Open']
-                        mainDf.at[index.Index, 'Initial Trading High'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['High']
-                        mainDf.at[index.Index, 'Initial Trading Low'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Low']
-                        mainDf.at[index.Index, 'Adj Close Day 1'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Adj Close']
-                        mainDf.at[index.Index, 'Volume Initial Trading Day'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Volume']
-                        daysToAddList = [7,30,90,180,365,730,1825,3650]
-                        for day in daysToAddList:
-                            mainDf = adjCloseCalc(mainDf,index,dataFromYahoo,day)
-            except:
-                #error with download
-                pass
-        mainDf.to_csv(os.getcwd()+'\\MainCsvs\\output.csv',index=False) #save for later viewing
+        mainDf = pandas.DataFrame()
+        try:
+            directory = os.getcwd() + '\\MainDataa'
+            for filename in os.listdir(directory):
+                     if filename.startswith("New issues"):
+                        print(filename)
+                        mainDf = pandas.read_excel(directory+'\\'+filename,header=7)
+        except:
+            print("Data file not found")
+
+        if mainDf.empty == False:
+            for index in mainDf.itertuples():
+                try:
+                   if index.TIDM != "": #if ticker exists
+                        dataFromYahoo = yfinance.download(index.TIDM+".L", period="max", repair=True, interval="1d",progress=False)
+                        if dataFromYahoo.empty:
+                            pass
+                        else:
+                            mainDf.at[index.Index, 'Initial Trading Open'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Open']
+                            mainDf.at[index.Index, 'Initial Trading High'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['High']
+                            mainDf.at[index.Index, 'Initial Trading Low'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Low']
+                            mainDf.at[index.Index, 'Adj Close Day 1'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Adj Close']
+                            mainDf.at[index.Index, 'Volume Initial Trading Day'] = dataFromYahoo.loc[dataFromYahoo.index.min()]['Volume']
+                            daysToAddList = [7,30,90,180,365,730,1825,3650]
+                            for day in daysToAddList:
+                                mainDf = adjCloseCalc(mainDf,index,dataFromYahoo,day)
+                except:
+                    #error with download
+                    pass
+            mainDf.to_csv(os.getcwd()+'\\MainData\\TickerData.csv',index=False) #save for later viewing
 
 
 main()
+
+#graveyard
+#pandas.set_option('display.max_columns', None)
+# df = pd.read_excel(os.getcwd() + '\\MainCsvs\\New issues and IPOs_37.xlsx', sheet_name='New issues and IPOs_37',header=7)
+# df.to_csv(os.getcwd() + '\\MainCsvs\\New issues and IPOs.csv', index=False)
--- dataCleaning.py
+++ dataCleaning.py
@@ -4,7 +4,7 @@ import os
 import pandas
 import yfinance
 import matplotlib
-
+from sklearn.feature_selection import SelectKBest, f_classif
 
 def getdatacsv():
     mainDf = pandas.read_csv(os.getcwd() + '\\MainCsvs\\output.csv', encoding='latin1', header=0)
@@ -14,7 +14,6 @@ def getdatacsv():
 def removecols(mainDf):
     mainDf = mainDf.drop(['Transfer', 'LSE IPO'], axis=1)
     return mainDf
-
 
 def checkfortypes(mainDf):
     print(mainDf.isna().sum(),"\n")
@@ -52,26 +51,80 @@ def removeanomalousrows(mainDf):
     return mainDf
 
 
+def removenonipo(mainDf):
+    rows_to_drop = mainDf[mainDf['LSE IPO'] == "Not IPO"].index
+    mainDf.drop(rows_to_drop, inplace=True)
+    return mainDf
+
+
+
+
+
+def datapreparation(mainDf):
+    mainDf = removecols(mainDf)
+    mainDf = removenonipo(mainDf)
+    # showrowswithmissingvals(mainDf)
+    mainDf = removeemptyrows(mainDf)
+    mainDf = removeanomalousrows(mainDf)
+    showrowswithmissingvals(mainDf)
+    # checkfortypes(mainDf)
+    # rowcount(mainDf)
+    #print(mainDf)
+
+
+    mainDf = exploratorydataanalysis(mainDf)
+
+    return mainDf
+
+
+def exploratorydataanalysis(mainDf):
+
+    mainDf['Currency'].value_counts().plot.bar()
+    #matplotlib.pyplot.show()
+
+    # mainDf['Initial Trading Open'].plot.bar()
+    matplotlib.pyplot.boxplot(mainDf['Initial Trading Open'], showmeans=True)
+    # print(mainDf['Initial Trading Open'].max())
+    # matplotlib.pyplot.figure()
+    #matplotlib.pyplot.show()
+
+    topfeatures(mainDf)
+    return mainDf
+
+
+def topfeatures(mainDf):
+    k = 1
+    for col in mainDf:
+        # Skip columns that are not numeric
+        if not pandas.api.types.is_numeric_dtype(mainDf[col]):
+            continue
+        print(mainDf[col])
+        # Create a SelectKBest object to select the top k features for the column
+        selector = SelectKBest(score_func=f_classif, k=k)
+
+        # Fit the selector to the data in the column
+        selector.fit(mainDf[[col]], mainDf['Adj Close Day 1'])
+
+        # Get the indices of the top k features for the column
+        top_k_indices = selector.get_support(indices=True)
+
+        # Get the names of the top k features for the column
+        top_k_features = mainDf[[col]].columns[top_k_indices]
+
+        # Print the results
+        print(f"Top {k} features for column '{col}': {list(top_k_features)}")
+
+
+
+
 def main():
     pandas.set_option('display.max_columns', None)
     pandas.set_option('display.width', 280)
-    mainDf = getdatacsv()
-    mainDf = removecols(mainDf)
-    #showrowswithmissingvals(mainDf)
-    mainDf = removeemptyrows(mainDf)
-    mainDf = removeanomalousrows(mainDf)
-    #showrowswithmissingvals(mainDf)
-    #checkfortypes(mainDf)
-    #rowcount(mainDf)
 
-    mainDf['Currency'].value_counts().plot.bar()
-    matplotlib.pyplot.show()
-    #mainDf['Initial Trading Open'].plot.bar()
-    matplotlib.pyplot.boxplot(mainDf['Initial Trading Open'],showmeans=True)
-    #print(mainDf['Initial Trading Open'].max())
-    #matplotlib.pyplot.figure()
-    matplotlib.pyplot.show()
-    #print(mainDf)
+    mainDf = getdatacsv()
+    mainDf = datapreparation(mainDf)
+
+
 
 
 
